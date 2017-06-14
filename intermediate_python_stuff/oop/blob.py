9ea98d9eeb260a05a7ea398bf80cfd6f63308a1d
raghav <raghavjindal2003@gmail.com>
2017-06-14 13:08:58 +0530

    Some minor changes

--- intermediate_python_stuff/oop/blob.py
+++ intermediate_python_stuff/oop/blob.py
@@ -5,7 +5,7 @@ import random
 
 
 class Blob:
-    def __init__(self, color, x_boundary, y_boundary, size_range=(4, 8), mov_range=(-1,2)):
+    def __init__(self, color, x_boundary, y_boundary, size_range=(4, 8), mov_range=(-1, 2)):
         # when object created first the init method is run
         self.color = color
         self.x_boundary = x_boundary
@@ -14,9 +14,11 @@ class Blob:
         self.y = random.randrange(0, self.y_boundary)
         self.size = random.randrange(size_range[0], size_range[1])
         self.mov_range = mov_range
+        self.move_x = 0
+        self.move_y = 0
 
     def move(self):
-        self.move_x = random.randrange(self.mov_range[0],  self.mov_range[1])
+        self.move_x = random.randrange(self.mov_range[0], self.mov_range[1])
         self.move_y = random.randrange(self.mov_range[0], self.mov_range[1])  # movements in pixel form
         self.x += self.move_x
         self.y += self.move_y
